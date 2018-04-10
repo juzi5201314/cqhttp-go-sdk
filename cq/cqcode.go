9eacb1a19f9f8778944913629e6acecc2a3f05a0
orange soeur <1034236490@qq.com>
2018-04-10 17:31:57 +0800

    Add files via upload

--- cq/cqcode.go
+++ cq/cqcode.go
@@ -3,57 +3,57 @@ package cq
 import "strconv"
 
 func At(qq string) string {
-return "[CQ:at,qq=" + qq + "]"
+	return "[CQ:at,qq=" + qq + "]"
 }
 
 func Face(faceid int) string {
-return "[CQ:face,id=" + string(faceid) + "]"
+	return "[CQ:face,id=" + string(faceid) + "]"
 }
 
 func Emoji(emojiid int) string {
-return "[CQ:emoji,id=" + string(emojiid) + "]"
+	return "[CQ:emoji,id=" + string(emojiid) + "]"
 }
 
 func Bface(bfaceid int) string {
-return "[CQ:bface,id=" + string(bfaceid) + "]"
+	return "[CQ:bface,id=" + string(bfaceid) + "]"
 }
 
 func Sface(sfaceid int) string {
-return "[CQ:sface,id=" + string(sfaceid) + "]"
+	return "[CQ:sface,id=" + string(sfaceid) + "]"
 }
 
 func Image(file string) string {
-return "[CQ:image,id=" + file + "]"
+	return "[CQ:image,id=" + file + "]"
 }
 
 func Record(file string, magic bool) string {
-return "[CQ:record,file=" + file + ",magic=" + strconv.FormatBool(magic) + "]"
+	return "[CQ:record,file=" + file + ",magic=" + strconv.FormatBool(magic) + "]"
 }
 
 func Rps() string {
-return "[CQ:rps]"
+	return "[CQ:rps]"
 }
 
 func Dice() string {
-return "[CQ:dice]"
+	return "[CQ:dice]"
 }
 
 func Shake() string {
-return "[CQ:Shake]"
+	return "[CQ:Shake]"
 }
 
 func Anonymous(ignore bool) string {
-return "[CQ:anonymous,ignore=" + strconv.FormatBool(ignore) + "]"
+	return "[CQ:anonymous,ignore=" + strconv.FormatBool(ignore) + "]"
 }
 
 func Music(_type string, id int) string {
-return "[CQ:music,type=" + _type + "，id=" + string(id) + "]"
+	return "[CQ:music,type=" + _type + "，id=" + string(id) + "]"
 }
 
 func CustomMusic(url string, audio string, title string, content string, image string) string {
-return "[CQ:music,type=custom,url=" + url + ",audio=" + audio + ",title=" + title + ",content=" + content + ",image=" + image + "]"
+	return "[CQ:music,type=custom,url=" + url + ",audio=" + audio + ",title=" + title + ",content=" + content + ",image=" + image + "]"
 }
 
 func Share(url string, title string, content string, image string) string {
-return "[CQ:share,url=" + url + ",title=" + title + ",content=" + content + ",image=" + image + "]"
+	return "[CQ:share,url=" + url + ",title=" + title + ",content=" + content + ",image=" + image + "]"
 }
