e7bcc1f9519ef881a1bb3121adae303917c7eff5
orange soeur <1034236490@qq.com>
2018-04-14 13:22:22 +0800

    Add files via upload

---
+++ server/httpserver.go
@@ -0,0 +1,81 @@
+package server
+
+import (
+	"net/http"
+	"strconv"
+	"io/ioutil"
+	"encoding/json"
+)
+
+func StartListenServer(port int, path string) ListenServer {
+	return ListenServer{port, path, EventListener{}}
+}
+
+type ListenServer struct {
+	port     int
+	path     string
+	listener EventListener
+}
+
+func (s *ListenServer) Listen() {
+	http.HandleFunc(s.path, func(writer http.ResponseWriter, request *http.Request) {
+		defer request.Body.Close()
+		buffer, _ := ioutil.ReadAll(request.Body)
+		m := map[string]interface{}{}
+		json.Unmarshal(buffer, &m)
+		rm := map[string]interface{}{}
+		switch m["post_type"] {
+		case "message":
+			rm = s.listener.onMessage(m)
+		case "event":
+			rm = s.listener.onEvent(m)
+		}
+
+		if rm == nil || len(rm) < 1 {
+			writer.WriteHeader(204)
+		} else {
+			jo, _ := json.Marshal(rm)
+			writer.Write(jo)
+		}
+	})
+	err := http.ListenAndServe(":"+strconv.Itoa(s.port), nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (s *ListenServer) ListenPrivateMessage(listener PrivateMessageListener) {
+	s.listener.private_message = listener
+}
+
+func (s *ListenServer) ListenGroupMessage(listener GroupMessageListener) {
+	s.listener.group_message = listener
+}
+
+func (s *ListenServer) ListenDiscussMessage(listener DiscussMessageListener) {
+	s.listener.discuss_message = listener
+}
+
+func (s *ListenServer) ListenGroupUpload(listener GroupUploadListener) {
+	s.listener.group_upload = listener
+}
+
+func (s *ListenServer) ListenGroupAdmin(listener GroupAdminListener) {
+	s.listener.group_admin = listener
+}
+
+func (s *ListenServer) ListenGroupDecrease(listener GroupDecreaseListener) {
+	s.listener.group_decrease = listener
+}
+
+func (s *ListenServer) ListenGroupIncrease(listener GroupIncreaseListener) {
+	s.listener.group_increase = listener
+}
+
+func (s *ListenServer) ListenFriendRequest(listener FriendRequestListener) {
+	s.listener.friend_request = listener
+}
+
+func (s *ListenServer) ListenGroupRequest(listener GroupRequestListener) {
+	s.listener.group_request = listener
+}
--- server/listener.go
+++ server/listener.go
@@ -1,1 +1,62 @@
+package server
 
+type PrivateMessageListener func(string, float64, float64, string, float64) map[string]interface{}
+type GroupMessageListener func(string, float64, float64, float64, string, float64, string, string) map[string]interface{}
+type DiscussMessageListener func(float64, float64, float64, string, float64) map[string]interface{}
+
+type GroupUploadListener func(float64, float64, map[string]interface{}) map[string]interface{}
+type GroupAdminListener func(string, float64, float64) map[string]interface{}
+type GroupDecreaseListener func(string, float64, float64, float64) map[string]interface{}
+type GroupIncreaseListener func(string, float64, float64, float64) map[string]interface{}
+
+type FriendRequestListener func(float64, string, string) map[string]interface{}
+type GroupRequestListener func(string, float64, float64, string, string) map[string]interface{}
+
+type EventListener struct {
+	private_message PrivateMessageListener
+	group_message GroupMessageListener
+	discuss_message DiscussMessageListener
+	group_upload GroupUploadListener
+	group_admin GroupAdminListener
+	group_decrease GroupDecreaseListener
+	group_increase GroupIncreaseListener
+	friend_request FriendRequestListener
+	group_request GroupRequestListener
+}
+
+func (el *EventListener) onMessage(m map[string]interface{}) map[string]interface{} {
+	switch m["message_type"] {
+	case "private":
+		return el.private_message(m["sub_type"].(string), m["message_id"].(float64), m["user_id"].(float64), m["message"].(string), m["font"].(float64))
+		break
+	case "grouop":
+		return el.group_message(m["sub_type"].(string), m["message_id"].(float64), m["group_id"].(float64), m["user_id"].(float64), m["message"].(string), m["font"].(float64), m["anonymous"].(string), m["anonymous_flag"].(string))
+	case "discuss":
+		return el.discuss_message(m["message_id"].(float64), m["discuss_id"].(float64), m["user_id"].(float64), m["message"].(string), m["font"].(float64))
+	}
+	return nil
+}
+
+func (el *EventListener) onEvent(m map[string]interface{}) map[string]interface{} {
+	switch m["event"] {
+	case "group_upload":
+		return el.group_upload(m["group_id"].(float64), m["user_id"].(float64), m["file"].(map[string]interface{}))
+	case "group_admin":
+		return el.group_admin(m["sub_type"].(string), m["group_id"].(float64), m["user_id"].(float64))
+	case "group_decrease":
+		return el.group_decrease(m["sub_type"].(string), m["group_id"].(float64), m["user_id"].(float64), m["operator_id"].(float64))
+	case "group_increase":
+		return el.group_increase(m["sub_type"].(string), m["group_id"].(float64), m["user_id"].(float64), m["operator_id"].(float64))
+	}
+	return nil
+}
+
+func (el *EventListener) onRequest(m map[string]interface{}) map[string]interface{} {
+	switch m["request_type"] {
+	case "friend":
+		return el.friend_request(m["user_id"].(float64), m["message"].(string), m["flag"].(string))
+	case "group":
+		return el.group_request(m["sub_type"].(string), m["group_id"].(float64), m["user_id"].(float64), m["message"].(string), m["flag"].(string))
+	}
+	return nil
+}
